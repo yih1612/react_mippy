f5e28eaeacdad03aa9ad6052e09b815dbf7687bb
yih1612 <yih1612@naver.com>
2023-06-20 16:29:37 +0900

    14.17 - new - UI

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, disabled }) {
   return (
     <button
       className="bg-brand py-1 px-2 text-white rounded-sm hover:brightness-110"
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -6,6 +6,8 @@ import { addNewProduct } from "../api/firebase";
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
+  const [isUploading, setIsUploading] = useState(false);
+  const [success, setSuccess] = useState();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -17,15 +19,31 @@ export default function NewProduct() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    uploadImage(file).then((url) => {
-      console.log(url);
-      addNewProduct(product, url);
-    });
+    setIsUploading(true);
+    uploadImage(file) //
+      .then((url) => {
+        addNewProduct(product, url) //
+          .then(() => {
+            setSuccess("성공적으로 제품이 추가되었습니다.");
+            setTimeout(() => {
+              setSuccess(null);
+            }, 3000);
+          });
+      })
+      .finally(() => setIsUploading(false));
   };
   return (
-    <section>
-      {file && <img src={URL.createObjectURL(file)} alt="local file" />}
-      <form onSubmit={handleSubmit}>
+    <section className="w-full text-center">
+      <h2 className="text-2xl font-bold my-4">새로운 제품 등록</h2>
+      {success && <p className="my-2">✅ {success}</p>}
+      {file && (
+        <img
+          className="w-96 mx-auto mb-2"
+          src={URL.createObjectURL(file)}
+          alt="local file"
+        />
+      )}
+      <form className="flex flex-col px-12" onSubmit={handleSubmit}>
         <input
           type="file"
           accept="image/*"
@@ -73,7 +91,10 @@ export default function NewProduct() {
           required
           onChange={handleChange}
         />
-        <Button text={"제품 등록하기"} />
+        <Button
+          text={isUploading ? "업로드중..." : "제품 등록하기"}
+          disabled={isUploading}
+        />
       </form>
     </section>
   );
