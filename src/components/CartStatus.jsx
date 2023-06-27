ff8799442efb6727c1e97e613f4317eba11c0477
yih1612 <yih1612@naver.com>
2023-06-27 17:05:21 +0900

    14.26 cart - Carts

--- src/components/CartStatus.jsx
+++ src/components/CartStatus.jsx
@@ -7,9 +7,10 @@ import { useAuthContext } from "../context/AuthContext";
 export default function CartStatus() {
   const { uid } = useAuthContext();
   const { data: products } = useQuery(["cart"], () => getCart(uid));
+
   return (
     <div className="relative">
-      <BsCart3 className="text-3xl" />
+      <BsCart3 className="text-2xl" />
       {products && (
         <p className="absolute -top-1 -right-2 w-5 h-5 bg-brand rounded-full text-sm text-center text-white font-semibold ">
           {products.length}
--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -1,5 +1,49 @@
 import React from "react";
+import { getCart } from "../api/firebase";
+import { useQuery } from "@tanstack/react-query";
+import { useAuthContext } from "../context/AuthContext";
+import CartItem from "../components/CartItem";
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import { FaEquals } from "react-icons/fa";
+import PriceCard from "../components/PriceCard";
+
+const SHIPPING = 3000;
 
 export default function Carts() {
-  return <div>carts</div>;
+  const { uid } = useAuthContext();
+  const { isLoading, data: products } = useQuery(["cart"], () => getCart(uid));
+
+  const hasProducts = products && products.length > 0;
+  const totalPrice =
+    hasProducts &&
+    products.reduce(
+      (prev, current) => prev + parseInt(current.price) * current.quantity,
+      0
+    );
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <section>
+      <p>내 장바구니</p>
+      {!hasProducts && <p>장바구니에 상품이 없습니다. 열심히 쇼핑해 주세요</p>}
+      {hasProducts && (
+        <>
+          <ul>
+            {products &&
+              products.map((product) => (
+                <CartItem key={product.id} product={product} uid={uid} />
+              ))}
+          </ul>
+          <div>
+            <PriceCard text="상품 총액" price={totalPrice} />
+            <BsFillPlusCircleFill />
+            <PriceCard text="배송액" price={SHIPPING} />
+            <FaEquals />
+            <PriceCard text="총가격" price={totalPrice + SHIPPING} />
+          </div>
+        </>
+      )}
+    </section>
+  );
 }
