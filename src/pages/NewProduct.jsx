3f6c34d89642bbf9c141e70d6916faefa61396a1
yih1612 <yih1612@naver.com>
2023-06-30 16:17:40 +0900

    14.28 real query

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -2,12 +2,21 @@ import React, { useState } from "react";
 import Button from "../components/ui/Button";
 import { uploadImage } from "../api/uploader";
 import { addNewProduct } from "../api/firebase";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
+
+  const queryClient = useQueryClient();
+  const addProduct = useMutation(
+    ({ product, url }) => addNewProduct(product, url),
+    {
+      onSuccess: () => queryClient.invalidateQueries(["products"]),
+    }
+  );
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -22,13 +31,17 @@ export default function NewProduct() {
     setIsUploading(true);
     uploadImage(file) //
       .then((url) => {
-        addNewProduct(product, url) //
-          .then(() => {
-            setSuccess("성공적으로 제품이 추가되었습니다.");
-            setTimeout(() => {
-              setSuccess(null);
-            }, 3000);
-          });
+        addProduct.mutate(
+          { product, url },
+          {
+            onSuccess: () => {
+              setSuccess("성공적으로 제품이 추가되었습니다.");
+              setTimeout(() => {
+                setSuccess(null);
+              }, 3000);
+            },
+          }
+        );
       })
       .finally(() => setIsUploading(false));
   };
