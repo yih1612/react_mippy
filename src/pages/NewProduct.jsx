48e636f5228a6143ef2deecb6bca4d06dd917a6c
yih1612 <yih1612@naver.com>
2023-06-30 21:05:16 +0900

    14.29 refactoring

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
 import Button from "../components/ui/Button";
 import { uploadImage } from "../api/uploader";
-import { addNewProduct } from "../api/firebase";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useProducts from "../hooks/useProducts";
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
-
-  const queryClient = useQueryClient();
-  const addProduct = useMutation(
-    ({ product, url }) => addNewProduct(product, url),
-    {
-      onSuccess: () => queryClient.invalidateQueries(["products"]),
-    }
-  );
+  const { addProduct } = useProducts();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -38,7 +30,7 @@ export default function NewProduct() {
               setSuccess("성공적으로 제품이 추가되었습니다.");
               setTimeout(() => {
                 setSuccess(null);
-              }, 3000);
+              }, 2000);
             },
           }
         );
